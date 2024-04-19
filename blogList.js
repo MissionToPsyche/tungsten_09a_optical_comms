a861f0fda27aeda7e80d64e5d07769af360dfa96
Niketan Chandarana <psychemission@gmail.com>
2024-04-19 13:05:13 -0700

    updated the order of blogs in the list

--- blogList.js
+++ blogList.js
@@ -1,16 +1,6 @@
 const blogList = [
     {
         author:{
-            name: 'Peyton Idleman',
-            imageSrc: 'static/peyton.jpg'
-        },
-        title: "Introducing the Team of Psyche's Tungsten Class",
-        publishedDate: 'March 25, 2024',
-        description: "The NASA Psyche mission, led by Arizona State University (ASU), includes a Student Collaborations program, with opportunities and senior design/capstone project experiences available to undergraduate students that want to get involved with its historic exploration. Every year, the Psyche mission uses a name of the elements that may be found on the Psyche asteroid to label each class of students that work with the Psyche team.",
-        link:'./blog1.html'
-    },
-    {
-        author:{
             name: 'Madison Hedges',
             imageSrc: 'static/madison.jpg'
         },
@@ -18,6 +8,16 @@ const blogList = [
         publishedDate: 'April 15, 2024',
         description: "Hello! In this article I’ll be talking about the ADC and microcontroller subsystem in the Psyche DSOC Detection Project. As an overview, ADC stands for ‘Analog to Digital Converter.’",
         link:'./blog2.html'
+    },
+    {
+        author:{
+            name: 'Peyton Idleman',
+            imageSrc: 'static/peyton.jpg'
+        },
+        title: "Introducing the Team of Psyche's Tungsten Class",
+        publishedDate: 'March 25, 2024',
+        description: "The NASA Psyche mission, led by Arizona State University (ASU), includes a Student Collaborations program, with opportunities and senior design/capstone project experiences available to undergraduate students that want to get involved with its historic exploration. Every year, the Psyche mission uses a name of the elements that may be found on the Psyche asteroid to label each class of students that work with the Psyche team.",
+        link:'./blog1.html'
     }
 ];
 
